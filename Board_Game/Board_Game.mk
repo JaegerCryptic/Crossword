f8e6b697c7cc34ed745ad573e212fee64875f4e6
Kyle <39253934+JaegerCryptic@users.noreply.github.com>
2020-04-14 21:54:09 +1000

    Fixed some shit and added others

--- Board_Game/Board_Game.mk
+++ Board_Game/Board_Game.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=Board_Game
 ConfigurationName      :=Debug
-WorkspacePath          :=C:/Users/Kyle/Documents/GitHub/Crossword
-ProjectPath            :=C:/Users/Kyle/Documents/GitHub/Crossword/Board_Game
+WorkspacePath          :=C:/Users/jaege/OneDrive/Documents/GitHub/Crossword
+ProjectPath            :=C:/Users/jaege/OneDrive/Documents/GitHub/Crossword/Board_Game
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=Kyle
-Date                   :=12/04/2020
-CodeLitePath           :="C:/Program Files (x86)/CodeLite"
-LinkerName             :="C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/g++.exe"
-SharedObjectLinkerName :="C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/g++.exe" -shared -fPIC
+User                   :=jaege
+Date                   :=14/04/2020
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/msys64/mingw64/bin/g++.exe
+SharedObjectLinkerName :=C:/msys64/mingw64/bin/g++.exe -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -36,7 +36,7 @@ ObjectsFileList        :="Board_Game.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=makedir
 RcCmpOptions           := 
-RcCompilerName         :="C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/windres.exe"
+RcCompilerName         :=C:/msys64/mingw64/bin/windres.exe
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -49,19 +49,19 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/ar.exe" rcu
-CXX      := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/g++.exe"
-CC       := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/gcc.exe"
+AR       := C:/msys64/mingw64/bin/ar.exe rcu
+CXX      := C:/msys64/mingw64/bin/g++.exe
+CC       := C:/msys64/mingw64/bin/gcc.exe
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/as.exe"
+AS       := C:/msys64/mingw64/bin/as.exe
 
 
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=C:\Program Files (x86)\CodeLite
+CodeLiteDir:=C:\Program Files\CodeLite
 Objects0=$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IntermediateDirectory)/boardGeneration.c$(ObjectSuffix) 
 
 
@@ -93,19 +93,15 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/main.c$(ObjectSuffix): main.c $(IntermediateDirectory)/main.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "C:/Users/Kyle/Documents/GitHub/Crossword/Board_Game/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/main.c$(DependSuffix): main.c
+$(IntermediateDirectory)/main.c$(ObjectSuffix): main.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.c$(ObjectSuffix) -MF$(IntermediateDirectory)/main.c$(DependSuffix) -MM main.c
-
+	$(CC) $(SourceSwitch) "C:/Users/jaege/OneDrive/Documents/GitHub/Crossword/Board_Game/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) main.c
 
-$(IntermediateDirectory)/boardGeneration.c$(ObjectSuffix): boardGeneration.c $(IntermediateDirectory)/boardGeneration.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "C:/Users/Kyle/Documents/GitHub/Crossword/Board_Game/boardGeneration.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/boardGeneration.c$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/boardGeneration.c$(DependSuffix): boardGeneration.c
+$(IntermediateDirectory)/boardGeneration.c$(ObjectSuffix): boardGeneration.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/boardGeneration.c$(ObjectSuffix) -MF$(IntermediateDirectory)/boardGeneration.c$(DependSuffix) -MM boardGeneration.c
-
+	$(CC) $(SourceSwitch) "C:/Users/jaege/OneDrive/Documents/GitHub/Crossword/Board_Game/boardGeneration.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/boardGeneration.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/boardGeneration.c$(PreprocessSuffix): boardGeneration.c
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/boardGeneration.c$(PreprocessSuffix) boardGeneration.c
 
--- Board_Game/boardGeneration.c
+++ Board_Game/boardGeneration.c
@@ -1,8 +1,63 @@
+void error()
+{
+    printf("Entered value was invalid. Please try again\n");
+}
+
+short toddlerProof(short userVal)
+{
+    while(1)
+    {
+        if(scanf("%hi", &userVal) == 0)
+        {
+            while(getchar() != '\n');   
+        }
+        else if(userVal >= 2)
+        {
+            return userVal;
+        }
+        error();
+    }
+}
+
+short dummyProof(short userVal)
+{
+    while(1)
+    {
+        if(scanf("%hi", &userVal) == 0)
+        {
+            while(getchar() != '\n');   
+        }
+        else if(userVal >= 2 && userVal <= 6)
+        {
+            return userVal;
+        }
+        error();
+    }
+}
+
+short babyProof(short userVal)
+{
+    while(1)
+    {
+        if(scanf("%hi", &userVal) == 0)
+        {
+            while(getchar() != '\n');   
+        }
+        else if(userVal == 1 || userVal == 2)
+        {
+            return userVal;
+        }
+        error();
+    }
+}
 
 static char randomSymbol(short inNoSymbols)
 {
-    int random = (rand() % inNoSymbols ) + 'A';
-    return (char)random;
+    int random = (rand() % inNoSymbols );
+    
+    char symbols[] = "!@#$%&";
+    
+    return symbols[random];
 }
 
 static void gridSize(short inNoSymbols)
@@ -12,8 +67,8 @@ static void gridSize(short inNoSymbols)
     
     char** grid;
     
-    printf("What size grid would you like?\n");
-    scanf("%hi", &totalGrid);
+    printf("What size grid would you like?. Must be greater than 1.\n");
+    totalGrid = toddlerProof(totalGrid);
     printf("\n");
     
     grid = (char **)malloc((totalGrid + 1) * sizeof(char *));
@@ -98,18 +153,19 @@ static void symbolSize()
     // determines no of symbols
     short noSymbols;
     
-    printf("How many symbols would you like? Warning. No more than 26 symbols. More than 6 symbols may result in a unbalanced game.\n");
-    scanf("%hi", &noSymbols);
+    printf("How many symbols would you like? Warning. The game supports up to 6 symbols.\n");
+    noSymbols = dummyProof(noSymbols);
     gridSize(noSymbols);
 }
 
 static int startGame()
 {
     // determines if new game or old
-    short gameSelect; 
+    short gameSelect;
     
-    printf("Would you like to start a new game(1) or load an old game(2). Please select 1 or 2\n");
-    scanf("%hi", &gameSelect);
+    printf("Would you like to start a new game(1), or load an old game(2). Please select 1 or 2\n");
+    
+    gameSelect = babyProof(gameSelect);
     
     if(gameSelect == 1)
     {
